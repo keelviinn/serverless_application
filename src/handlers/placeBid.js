389df4736016491ea1e78399c958dfddb66cb3d0
Kelvin Oliveira <keelviinn@gmail.com>
2021-03-18 02:04:31 -0300

    add validation for place bid

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -9,16 +9,17 @@ async function placeBid(event, context) {
   let updatedAuction;
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
-  if (auction.status !== 'OPEN') 
-    throw new createError.Forbidden(`You cannot bid on closed auctions - Auction Status: ${auction.status}`)
-  if (amount <= auction.highestBid.amount) 
-    throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`)
+  if (auction.seller === email) throw new createError.Forbidden(`You cannot bid your own Auction`)
+  if (auction.highestBid.bidder === email) throw new createError.Forbidden(`You are already the highest bidder`)
+  if (auction.status !== 'OPEN') throw new createError.Forbidden(`You cannot bid on closed auctions - Auction Status: ${auction.status}`)
+  if (amount <= auction.highestBid.amount) throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`)
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id }, 
-    UpdateExpression: 'set highestBid.amount = :amount',
-    ExpressionAttributeValues: { ':amount': amount },
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
+    ExpressionAttributeValues: { ':amount': amount, ':bidder': email },
     ReturnValues: 'ALL_NEW'
   }
   try {
